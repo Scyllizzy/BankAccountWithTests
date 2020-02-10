bee1a4dd71c80f467ea5e4027634a421ff44f373
Alexia Haley <haley7734@student.cptc.edu>
2020-02-10 14:35:57 -0800

    Added constructor and deposit tests

--- BankAccountWithTests.Tests/BankAccountTests.cs
+++ BankAccountWithTests.Tests/BankAccountTests.cs
@@ -16,14 +16,89 @@ namespace BankAccountWithTests.Tests
         {
             //Arrange - create objects and variables
             BankAccount acc = new BankAccount("U030043664");
-            double depositAmount = 100.0;
-            double expectedBalance = 100.0;
+            double depositAmount = 100.00;
+            double expectedBalance = 100.00;
 
             //Act - Call the method under test
             acc.Deposite(depositAmount);
 
             //Assert - Did what we want happen?
             Assert.AreEqual(expectedBalance, acc.Balance);
+        }
+
+        [TestMethod]
+        public void Deposit_PosititveAmountWithCents_AddsToBalance()
+        {
+            // Arrange
+            BankAccount acc = new BankAccount("U030043664");
+            double depositAmount = 10.55;
+            double expectedBalance = 10.55;
+
+            // Act
+            acc.Deposite(depositAmount);
+
+            // Assert
+            Assert.AreEqual(expectedBalance, acc.Balance);
+        }
+
+
+        [TestMethod]
+        [DataRow(100.00)]
+        [DataRow(100.99)]
+        [DataRow(0.01)]
+        [DataRow(9999999999.99)]
+        public void Deposit_PosititveAmount_ReturnsUpdatedBalance(double depositAmount)
+        {
+            // Arrange
+            BankAccount acc = new BankAccount("U030043664");
+            double initialBalance = 0;
+            double expectedBalance = initialBalance + depositAmount;
+
+            // Act
+            double retrunedBalance = acc.Deposite(depositAmount);
+
+            // Assert
+            Assert.AreEqual(expectedBalance, retrunedBalance);
+        }
+
+        [TestMethod]
+        [DataRow(-1)]
+        [DataRow(-1.99)]
+        [DataRow(0)]
+        [DataRow(-0.01)]
+        public void Deposit_NegativeAmount_ThrowsArgumentOutOfRangeException(double depositAmount)
+        {
+            // Arrange
+            BankAccount acc = new BankAccount("U030043664");
+
+            // Act => Assert
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => acc.Deposite(depositAmount));
+        }
+
+        [TestMethod]
+        [DataRow("123")]
+        [DataRow("a")]
+        [DataRow("1234567890")]
+        [DataRow("1dD")]
+        [DataRow("`~!@#$%^&*()_+-={}|[]\\:\";'<>?,./")]
+        [DataRow("(╯°□°)╯︵ ┻━┻")]
+        public void Constructor_ValidAccountNumber_SetsAccountNumber(string accountNumber)
+        {
+            // Arrange / Act
+            BankAccount acc = new BankAccount(accountNumber);
+
+            // Assert
+            Assert.AreEqual(accountNumber, acc.AccountNumber);
+        }
+
+        [TestMethod]
+        [DataRow(null)]
+        [DataRow("")]
+        [DataRow(" ")]
+        public void Constructor_InValidAccountNumber_ThrowsArgumentException(string accountNumber)
+        {
+            // Arrange / Act / Assert
+            Assert.ThrowsException<ArgumentException>(() => new BankAccount(accountNumber));
         }
     }
 }
